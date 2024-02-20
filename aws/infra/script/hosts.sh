eeeffcfdb80a4ca81b7671f8aa863ac132461259
Ricardo Fahham <rfahham@hotmail.com>
2024-02-20 16:20:47 -0300

    add script

---
+++ aws/infra/script/hosts.sh
@@ -0,0 +1,8 @@
+
+sudo su
+cat /etc/ansible/hosts
+echo "" > /etc/ansible/hosts
+echo "[instances]" >> /etc/ansible/hosts
+echo "ec2-user@ec2-<IP>.compute-1.amazonaws.com" >> /etc/ansible/hosts
+echo "ec2-user@ec2-<IP>.compute-1.amazonaws.com" >> /etc/ansible/hosts
+cat /etc/ansible/hosts
